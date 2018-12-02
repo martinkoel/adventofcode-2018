36c89ebcdef14ae43b96f0bbfa75e7f05cfa6786
martin <mkoel@bitcab.com>
2018-12-02 19:00:53 +0100

    day one, second puzzle

---
+++ src/main/java/nl/takmk/Day0102.java
@@ -0,0 +1,49 @@
+package nl.takmk;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day0102 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        ArrayList<Integer> input = readFile("input-day01-01.txt");
+        ArrayList<Integer> frequencies = new ArrayList<>();
+
+        int num01 = 0;
+        int currentFrequency;
+        boolean found = false;
+
+        while (!found) {
+            System.out.println("next round");
+            for (Integer cur : input) {
+                currentFrequency = num01 + cur.intValue();
+
+                if (frequencies.contains(currentFrequency)) {
+                    System.out.println("First occurrence of same frequency is: " + currentFrequency);
+                    found = true;
+                    break;
+                }
+
+                num01 = currentFrequency;
+                frequencies.add(currentFrequency);
+            }
+        }
+    }
+
+    private static ArrayList<Integer> readFile(String fileName) throws FileNotFoundException {
+        ArrayList<Integer> input = new ArrayList<Integer>();
+        ClassLoader classLoader = Day0102.class.getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine()) {
+            input.add(Integer.valueOf(scanner.nextLine()));
+        }
+
+        return input;
+    }
+}
